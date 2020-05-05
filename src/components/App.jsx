9d0016f87b9f766f2c5b0130b8f0d64b75c53f26
Rishab Munot <47574041+RishabMunot@users.noreply.github.com>
2020-05-06 00:30:26 +0530

    counter ready
    this counter uses useState from React

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function App() {
-  return <div />;
+  var [counter, setState] = React.useState(0);
+
+  function increase() {
+    setState(counter + 1);
+  }
+
+  function decrease() {
+    setState(counter - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{counter}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
